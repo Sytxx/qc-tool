9d4fdb83fe2b8631e9b6b1848a7f5ca9cb4dbaa0
Sytxx <56584493+Sytxx@users.noreply.github.com>
2023-09-11 22:37:45 -0700

    Update aio-tools.bat

--- aio-tools.bat
+++ aio-tools.bat
@@ -1,1 +1,13 @@
-REM : This is only testing 
+@echo off
+REM : Project ini dibuat untuk memenuhi kebutuhan, dan project ini open source.
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::
+
+if not exist "./requirement" goto norequirement
+cd sytx
+
+:menuawal
+chcp 65001 >nul 2>&1
+cls
+set choice=
+set /p choice=Your choice:
