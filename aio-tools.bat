38b3a84a483a0267e9c3a688580cca199a940bc1
Sytxx <56584493+Sytxx@users.noreply.github.com>
2023-09-11 22:52:24 -0700

    Update aio-tools.bat
    adding reg edit for collored ascii

--- aio-tools.bat
+++ aio-tools.bat
@@ -1,10 +1,13 @@
 @echo off
 REM : Project ini dibuat untuk memenuhi kebutuhan, dan project ini open source.
 
-:::::::::::::::::::::::::::::::::::::::::::::::::::
-
+REM : Jangan diubah code dibawah ini, nanti error.
+ 
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (set "DEL=%%a" & set "COL=%%b")
 if not exist "./requirement" goto norequirement
 cd sytx
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 :menuawal
 chcp 65001 >nul 2>&1
